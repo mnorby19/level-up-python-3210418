ebc639963647f94bd2673753785d45f912f74639
mnorby19 <129117659+mnorby19@users.noreply.github.com>
2024-09-12 22:46:12 +0000

    Y

---
+++ src/01 Find Prime Factors/solution.py
@@ -0,0 +1,16 @@
+import math
+
+def get_prime_factors(num):
+  returnPrimes = []
+  devisor = 2
+  workingNum = num
+
+  while devisor <= workingNum:
+    if workingNum % devisor == 0:
+      returnPrimes.append(devisor)
+      workingNum = workingNum // devisor
+    else:
+      devisor += 1
+  return(returnPrimes)
+
+print(get_prime_factors(630))
